494b0b594a72ae9423888b648e1670ae48ed8bd3
Adriana1Leon <e15684@ism.edu.ec>
2026-04-13 23:57:32 -0500

    Update Ejercicio 1.py

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,1 +1,33 @@
+#Versión de Python (No sé como)
+
+
+#Operaciones con 3 y 4
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print( 3 / 4)
+print(3 ** 4)
+print( 3 // 4)
+
+#cadenas interactivas
+nombre = "Adriana"
+apellido = "Leon"
+pais = "Ecuador"
+frase = "Estoy disfrutando de 30 días de Python"
+
+print("\nCadenas:")
+print(nombre)
+print(apellido)
+print(pais)
+print(frase)
+#Tipos de datos
+print("\nTipos de datos:")
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(apellido))
+print(type(pais))
 
