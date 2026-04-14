ea86bac44e7b7763d84c48bd198e3bfda0ff12de
Adriana1Leon <e15684@ism.edu.ec>
2026-04-14 12:37:14 -0500

    Primer cambio

--- Actividad2(Variables).py
+++ Actividad2(Variables).py
@@ -1,1 +1,23 @@
+print(len("Adriana Leon"))
 
+#%%
+#Primera forma de hacer el ejrcicio
+first_name= "Adriana"
+longitud= len(first_name)
+print("First name lenght:",longitud)
+
+#%%
+#Segunda forma de hacer el ejercicio
+first_name= "Adriana"
+print("First name lenght:",len(first_name))
+
+#%%
+print(type(first_name))
+print(type(longitud))
+#%%
+
+#Multiples variables en una sola línea
+primer_nombre, apellido, pais, edad, estado_civil= "Adriana","Leon", "Ecuador", "16 años", "Soltera"
+
+print(primer_nombre, pais)
+# %%
