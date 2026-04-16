6367bc0996cdc5bd9a4bf51eeb6a93cc73e6febd
Adriana1Leon <e15684@ism.edu.ec>
2026-04-16 14:10:21 -0500

    ejercicios del 1-6

---
+++ Actividad3(Operadores).py
@@ -0,0 +1,22 @@
+#%%
+#Declara tu edad como una variable de tipo entero.  
+edad= int(16)
+#%%
+#Declara tu estatura como una variable de tipo decimal (float).  
+estatura= float (1.65)
+#%%
+#Escribe un programa que solicite al usuario la base y la altura de un triángulo y calcule su área 
+base= float(input ("Ingrese la base del triángulo: "))
+altura= float(input ("Ingrese la altura del triángulo: "))
+area= (0.5 * base * altura)
+print("El valor del área es:", area)
+#%%
+#Escribe un programa que solicite al usuario los lados a, b y c de un triángulo y calcule su perímetro
+lado_a= float (input("Ingrese el lado a: "))
+lado_b= float (input("Ingrese el lado b: "))
+lado_c= float (input("Ingrese el lado c: "))
+perimetro= (lado_a + lado_b + lado_c)
+print("El perimetro de su triángulo es: ", perimetro)
+#%%
+#Obtén la longitud y el ancho de un rectángulo usando un prompt. Calcula su área y su perímetro 
+Longitud= float (input("Ingrese el largo: "))
