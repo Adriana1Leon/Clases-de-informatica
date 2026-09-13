c103ff493cb7ac5a1f5fa87168eb87dede2d63b3
Adriana1Leon <e15684@ism.edu.ec>
2026-09-13 17:37:38 -0500

    Con/sin parámetros

---
+++ Funciones.py
@@ -0,0 +1,12 @@
+#FUNCIONES CON PARÁMETROS
+def saludar (nombre):
+    print(f"Hola, {nombre}")
+
+saludar("Adriana")
+
+#FUNCIONES SIN PARÁMETROS
+
+def saludar(): 
+    print("Hi, Adri")
+
+print(saludar())
